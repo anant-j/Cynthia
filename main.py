3651bb7bc76abfe7e208c29939d38f876401dcb6
AloysPortafaix <aloys.portafaix@gmail.com>
2020-02-01 14:41:36 -0500

    endpoint first commit

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def request():
+    return 'this is a post request'
+
+
+@app.route('/', methods=['GET'])
+def receive():
+    return 'this is get request'
+
+
+if __name__ == '__main__':
+    app.run()
