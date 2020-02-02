98dccc9101643edbcc204140065c5fac5d16e3ed
Anant Jain <anant.j2409@gmail.com>
2020-02-02 03:30:23 -0500

    Changes:

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 from flask import Flask
+from flask import request
+import json
 import requests
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def send():
+    return (json.dumps({"response": str("Please check console for results!")}))
 
 @app.route('/', methods=['POST'])
-def request():
-    return 'this is a post request'
-
-
-@app.route('/', methods=['GET'])
 def receive():
     # return("Hello")
     headers = {
         'Content-Type': 'application/json',
     }
     d={}
-    data = open('tone.json', 'rb').read()
+    # data = open('tone.json', 'rb').read()
+    inp_data = str(request.form['request'])
+    x = {
+  "text": inp_data
+    }
+# convert into JSON:
+    data = json.dumps(x)
+    print(data)
     response = requests.post(
-        'url',
+        'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/9decad53-7281-4d48-8c86-f105d1f42122/v3/tone?version=2017-09-21',
         headers=headers, data=data, auth=('apikey', 'key'))
     for responses in response.json()['document_tone']['tones']:  
         d[str(responses['tone_id'])]=responses['score']
-    return(str(d))
+    print(str(d))
+    return (json.dumps({"response": str("Please check console for results!")}))
 
 
 if __name__ == '__main__':
